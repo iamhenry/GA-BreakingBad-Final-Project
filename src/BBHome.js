6ef4c262fba4116744842563257e3a2c1ccb001c
Henry Moran <virtualhenry@gmail.com>
2020-01-22 16:15:41 -0800

    Adding useReducer for fetching home characters

--- src/BBHome.js
+++ src/BBHome.js
@@ -3,30 +3,56 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
 
-function BBHome() {
+function reducer(prevState, { action, payload }) {
+  console.log(prevState);
+  console.log(payload);
+  switch (action) {
+    case "setItems":
+      const newState = { ...prevState };
+      newState.items = payload;
+      return newState;
+    default:
+      return prevState;
+  }
+}
 
+function BBHome() {
   // const [state, dispatch] = useReducer(reducer, initialState);
   // TODO: Reducer
-  useEffect(() => {
-    fetchItems();
-  }, []);
 
-  const [items, setItems] = useState([]);
+  // useEffect(() => {
+  //   fetchItems();
+  // }, []);
 
-  const fetchItems = async () => {
-    const data = await fetch(
-      "https://www.breakingbadapi.com/api/characters?limit=9"
-    );
+  // const [items, setItems] = useState([]);
+  const [state, dispatch] = React.useReducer(reducer, {
+    items: []
+  });
 
-    const items = await data.json();
-    console.log(items);
-    setItems(items);
-  };
+  // const fetchItems = async () => {
+  //   const data = await fetch(
+  //     "https://www.breakingbadapi.com/api/characters?limit=9"
+  //   );
+
+  //   const items = await data.json();
+  //   console.log(items);
+  //   setItems(items);
+  // };
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://www.breakingbadapi.com/api/characters?limit=9`
+      );
+      const incomingData = await response.json();
+      console.log(incomingData)
+      dispatch({ action: "setItems", payload: incomingData });
+    })();
+  });
 
   return (
     <>
       <ul>
-        {items.map(item => (
+        {state.items.map(item => (
           <Link to={`/BBHome/${item.char_id}`}>
             <li key={item.char_id} className="bbchar">
               <img src={item.img} alt={item.name} />
--- src/Reducer.js
+++ src/Reducer.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + 1 };
+function reducer(prevState, { action, payload }) {
+  console.log(prevState);
+  switch (action) {
+    case "setItems":
+      const newState = { ...prevState };
+      newState.items = payload;
+      return newState;
     default:
-      throw new Error();
+      return prevState;
   }
 }
+
+const [state, dispatch] = React.useReducer(reducer, {
+  items: []  
+});
+
+React.useEffect(() => {
+  (async () => {
+    const response = await fetch(
+      `https://www.breakingbadapi.com/api/characters?limit=9`
+    );
+    const incomingData = await response.json();
+    dispatch({ action: "setItems", payload: incomingData.file });
+  })();
+});
